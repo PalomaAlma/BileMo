8ac86fd410878ca6ef51385d71ca56fe45561a2e
PalomaAlma <palomavacheron@gmail.com>
2022-11-09 15:32:00 +0100

    JMS Serializer installation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,10 +11,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\SerializerInterface;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
@@ -27,7 +30,7 @@ class UserController extends AbstractController
     public function index(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
     {
 
-        $jsonUser = $serializer->serialize($userRepository->findAll(), 'json', ['groups' => 'getUsers']);
+        $jsonUser = $serializer->serialize($userRepository->findAll(), 'json');
 
         return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
     }
@@ -49,7 +52,8 @@ class UserController extends AbstractController
 
             $userList = $userRepository->findAllWithPagination($page, $limit, $id);
 
-            return $serializer->serialize($userList, 'json', ['groups' => 'getUsers']);
+            $context = SerializationContext::create()->setGroups(['getUsers']);
+            return $serializer->serialize($userList, 'json', $context);
         });
 
         return new JsonResponse($jsonUserList, Response::HTTP_OK, [], true);
@@ -60,8 +64,8 @@ class UserController extends AbstractController
      */
     public function getDetailUser(UserRepository $userRepository, SerializerInterface $serializer, $id): JsonResponse
     {
-        $user = $userRepository->findOneById($id);
-        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+        $context = SerializationContext::create()->setGroups(['getUsers']);
+        $jsonUser = $serializer->serialize($userRepository->findOneById($id), 'json', $context);
 
         return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
     }
@@ -70,17 +74,19 @@ class UserController extends AbstractController
     /**
      * @Route("/api/users/new", name="user_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, UserRepository $userRepository, ClientRepository $clientRepository, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator): JsonResponse
+    public function new(Request $request,UserPasswordHasherInterface $userPasswordHasher, UserRepository $userRepository, ClientRepository $clientRepository, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator): JsonResponse
     {
         $user = $serializer->deserialize($request->getContent(), User::class, 'json');
 
+        $user->setPassword($userPasswordHasher->hashPassword($user, "password"));
         $content = $request->toArray();
-        $client = $content['client'] ?? -1;
+        $client = $content['client_id'] ?? -1;
         $user->setClient($clientRepository->findOneById($client));
 
         $userRepository->add($user, true);
 
-        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+        $context = SerializationContext::create()->setGroups(['getUsers']);
+        $jsonUser = $serializer->serialize($user, 'json', $context);
 
         $location = $urlGenerator->generate('detailUser', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
 
@@ -90,25 +96,27 @@ class UserController extends AbstractController
     /**
      * @Route("/api/users/{id}", name="user_edit", methods={"PUT"})
      */
-    public function edit(Request $request, UserRepository $userRepository, ClientRepository $clientRepository, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator, $id, TagAwareCacheInterface $cachePool): JsonResponse
+    public function edit(Request $request, UserRepository $userRepository, User $currentUser, ClientRepository $clientRepository, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator, $id, TagAwareCacheInterface $cachePool, UserPasswordHasherInterface $userPasswordHasher): JsonResponse
     {
-        $cachePool->invalidateTags(["usersCache"]);
-        $updatedUser = $serializer->deserialize($request->getContent(),
-            User::class,
-            'json',
-            [AbstractNormalizer::OBJECT_TO_POPULATE => $userRepository->findOneById($id)]);
+        $updatedUser = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $currentUser->setEmail($updatedUser->getEmail());
+        $currentUser->setPassword($userPasswordHasher->hashPassword($currentUser, $updatedUser->getPassword()));
+
+        // On vérifie les erreurs
+        /*$errors = $validator->validate($currentUser);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }*/
 
         $content = $request->toArray();
-        $client = $content['client'] ?? -1;
-        $updatedUser->setClient($clientRepository->findOneById($client));
+        $client = $content['client_id'] ?? -1;
+        $currentUser->setClient($clientRepository->findOneById($client));
 
-        $userRepository->add($updatedUser, true);
+        $userRepository->add($currentUser, true);
 
-        $jsonUser = $serializer->serialize($updatedUser, 'json', ['groups' => 'getUsers']);
+        $cachePool->invalidateTags(["usersCache"]);
 
-        $location = $urlGenerator->generate('detailUser', ['id' => $updatedUser->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
-
-        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ["Location" => $location], true);
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
     /**
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,7 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Serializer\Annotation\Groups;
+use JMS\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
