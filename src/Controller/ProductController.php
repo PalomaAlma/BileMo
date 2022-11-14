a3ba65da04706e34131a46b90dd987ad535c9a2e
PalomaAlma <palomavacheron@gmail.com>
2022-11-14 19:07:53 +0100

    Nelmio Doc

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +20,31 @@ use Symfony\Contracts\Cache\TagAwareCacheInterface;
 class ProductController extends AbstractController
 {
     /**
+     * Méthode permettant de récupérer l'ensemble des produits BileMo
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne la liste des produits",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Product::class))
+     *     )
+     * )
+     * @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="La page que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     *
+     * @OA\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     description="Le nombre d'éléments que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     * @OA\Tag(name="Produits")
+     *
      * @Route("/api/products", name="product", methods={"GET"})
      */
     public function getProductList(ProductRepository $productRepository, Request $request, TagAwareCacheInterface $cachePool, SerializerInterface $serializer): JsonResponse
@@ -40,6 +67,40 @@ class ProductController extends AbstractController
     }
 
     /**
+     * Méthode permettant de récupérer les détails d'un produit BileMo
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne les détails d'un produit",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Product::class))
+     *     )
+     * )
+     *
+     * @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="La page que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     *
+     * @OA\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     description="Le nombre d'éléments que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     *
+     * @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     description="L'id du produit que l'on veut récupérer",
+     *     @OA\Schema(type="string")
+     * )
+     *
+     * @OA\Tag(name="Produits")
+     *
      * @Route("/api/products/{id}", name="detailProduct", methods={"GET"})
      */
     public function getDetailProduct(Product $product): JsonResponse
